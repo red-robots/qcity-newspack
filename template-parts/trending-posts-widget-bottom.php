37286968ac6260500a5136beba35f4e5ceb7c3ed
Lisa DeBona <annalisa.quiamco@gmail.com>
2022-03-01 22:59:56 -0500

    Fixed trending posts on single post

--- template-parts/trending-posts-widget-bottom.php
+++ template-parts/trending-posts-widget-bottom.php
@@ -5,7 +5,6 @@ if( do_shortcode('[wpp range="last5days" thumbnail_width=300 thumbnail_height=20
   <div class="trending-widget-bottom">
   	<div class="widget-inner">
   		<h3 class="sbTitle">Trending</h3>
-			<?php //echo do_shortcode('[wpp range="last24hours" thumbnail_width=300 thumbnail_height=200 limit=5 stats_views=0 order_by="views"]'); ?>
       <?php echo do_shortcode('[wpp range="last24hours" thumbnail_width=300 thumbnail_height=200 limit=5 stats_views=0 order_by="views"]'); ?>
   	</div>		
   </div>
